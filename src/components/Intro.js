910adfdb7b8b40e4bedd135cdb096f0032889e3f
Farida-Hamid <faridaaddala@gmail.com>
2023-02-26 12:49:44 +0200

    Add content to the intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <h2>Intro</h2>
+    <>
+      <p>Hi, my name is</p>
+      <h1>Farida Hamid</h1>
+      <h1>A Full Stack Developer</h1>
+      <p>I'm a software engineer with an AI background. 
+      After learning so much last year, I'm longing to put it to practice and learn some more.
+      I enjoy connecting with people and build cool products.</p>
+    </>
   )
 }
 
