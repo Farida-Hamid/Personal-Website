cb22335ed7e3b5a5f61f8c01bf9245538bf64d90
Farida-Hamid <faridaaddala@gmail.com>
2023-02-28 16:27:00 +0200

    Display project card for each project

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProjectCard = ({id}) => {
+  
+  return (
+    <h2 style={{color: "var(--lightest-slate)"}}>Project.id  = {id}</h2>
+  )
+};
+
+export default ProjectCard;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,25 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 import "../style/projects.css";
 
 const Projects = () => {
+
+  const list = [{
+    id: 1
+  },
+  {
+    id: 2
+  }, 
+  {
+    id: 3
+  }];
+
     return (
       <>
-        <h1>projects</h1>
+        <h1 style={{color: "var(--lightest-slate)"}}>Projects</h1>
+        {list.map((project) => (
+          <ProjectCard key={project.id} />
+        ))}
       </>
     );
 };
