c49af6bbb9a62780aeeb504d22b6041614281476
Farida-Hamid <faridaaddala@gmail.com>
2023-02-28 17:22:09 +0200

    Display a dumy project data project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,26 @@
 import React from "react";
+// import PropTypes from 'prop-types';
 
-const ProjectCard = ({id}) => {
+const ProjectCard = ({ name, description, tech, liveLink, githubLink }) => {
   
   return (
-    <h2 style={{color: "var(--lightest-slate)"}}>Project.id  = {id}</h2>
+    <div style={{color: "var(--lightest-slate)"}}>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <ul>
+        {tech.map ((item, index) => (
+          <li key={index}> <div className="robotic">▷</div> {item}</li>
+        ))}
+      </ul>
+
+      <a href={liveLink} >Live</a><br/>
+      <a href={githubLink} >Github</a>
+    </div>
   )
 };
+
+// ProjectCard.propTypes = {
+//   key: PropTypes.number.isRequired
+// }
 
 export default ProjectCard;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,20 +5,42 @@ import "../style/projects.css";
 const Projects = () => {
 
   const list = [{
-    id: 1
+    id: 1,
+    name: 'Muvi-WebApp',
+    description: "This project is a web app about Movies, we fetched all data from TMBD API, Users can like movies and also give a comment. All data are recorded in the involvement API and also displayed on the screen.",
+    tech: ["HTML", "Javascript", "CSS", "Webpack", "Jest", "NPM"],
+    liveLink: "https://farida-hamid.github.io/Muvi-WebApp/",
+    githubLink: "https://farida-hamid.github.io/Muvi-WebApp/",
   },
   {
-    id: 2
+    id: 2,
+    name: 'Muvi-WebApp2',
+    description: "This project is a web app about Movies, we fetched all data from TMBD API, Users can like movies and also give a comment. All data are recorded in the involvement API and also displayed on the screen.",
+    tech: ["HTML", "Javascript", "CSS", "Webpack", "Jest", "NPM"],
+    liveLink: "https://farida-hamid.github.io/Muvi-WebApp/",
+    githubLink: "https://farida-hamid.github.io/Muvi-WebApp/",
   }, 
   {
-    id: 3
+    id: 3,
+    name: 'Muvi-WebApp3',
+    description: "This project is a web app about Movies, we fetched all data from TMBD API, Users can like movies and also give a comment. All data are recorded in the involvement API and also displayed on the screen.",
+    tech: ["HTML", "Javascript", "CSS", "Webpack", "Jest", "NPM"],
+    liveLink: "https://farida-hamid.github.io/Muvi-WebApp/",
+    githubLink: "https://farida-hamid.github.io/Muvi-WebApp/",
   }];
 
     return (
       <>
         <h1 style={{color: "var(--lightest-slate)"}}>Projects</h1>
         {list.map((project) => (
-          <ProjectCard key={project.id} />
+          <ProjectCard 
+            key={project.id}
+            name={project.name}
+            description={project.description}
+            tech={project.tech}
+            liveLink={project.liveLink}
+            githubLink={project.githubLink}            
+          />
         ))}
       </>
     );
