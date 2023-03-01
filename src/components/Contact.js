ad23d3a96e7d9513521efa3519106ca2afd14894
Farida-Hamid <faridaaddala@gmail.com>
2023-03-01 13:42:37 +0200

    Adjust spacing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,15 +4,15 @@ import '../style/contact.css';
 const Contact = () => {
   return (
     <>
-      <h2 className="title">
-        <div className="robotic">03.</div>
-        Contacts
+      {/* <h2 className="title">
+        <div className="robotic"></div>
+        Contact
         <hr></hr>
-      </h2>
+      </h2> */}
 
       <div className="call">
-        <p className="robotic">What’s Next?</p>
-        <h1 style={{color: "var(--lightest-slate)"}} >Get In Touch</h1>
+        <p className="robotic">03. What’s Next?</p>
+        <h1 className="contact-title">Get In Touch</h1>
         <p>I’m not currently looking for any new opportunities, my inbox is
           always open.
           Whether you have a question or just want to say hi, I would love to have a 
