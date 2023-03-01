2ed8171a55b6cb4c7212ce4cc9a0b2b04a9c5ab9
Farida-Hamid <faridaaddala@gmail.com>
2023-03-01 10:45:11 +0200

    MAke project section responsive

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,21 +1,24 @@
 import React from "react";
 
-const ProjectCard = ({ id, name, description, tech, liveLink, githubLink, image }) => {
+const ProjectCard = ({ name, description, tech, liveLink, githubLink, image }) => {
 
   return (
     <div className="project-card" style={{color: "var(--slate)"}}>
       <img className="screenshot" src={image} alt="screenshot" />
-      <h2 style={{color: "var(--lightest-slate)"}}>{name}</h2>
-      <p>{description}</p>
+      
+      <div className="project-info">
+        <h2 style={{color: "var(--lightest-slate)"}}>{name}</h2>
+        <p>{description}</p>
 
-      <ul className="robotic">
-        {tech.map ((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+        <ul className="tech">
+          {tech.map ((item, index) => (
+            <li className="robotic" key={index}>{item}</li>
+          ))}
+        </ul>
 
-      <a href={liveLink} >Live</a><br/>
-      <a href={githubLink} >Github</a>
+        <a href={liveLink} >Live</a><br/>
+        <a href={githubLink} >Github</a>
+      </div>
     </div>
   )
 };
