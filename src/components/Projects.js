69905a05ca60fdaec8407469f775834d6fa1d896
Farida-Hamid <faridaaddala@gmail.com>
2023-03-02 15:01:13 +0200

    Adjust picture sizes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,12 +15,12 @@ const Projects = () => {
   },
   {
     id: 2,
-    name: 'Maths',
+    name: 'Math Magicians',
     description: "Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations and read a random math-related quote. By building this application, you will learn how to use React.",
     tech: ["React", "Javascript", "CSS"],
     liveLink: "https://dev--adorable-churros-0bd093.netlify.app",
     githubLink: "https://github.com/Farida-Hamid/Maths",
-    image: 'https://github.com/Farida-Hamid/Personal-Website/blob/dev/src/style/assets/project1.png?raw=true',
+    image: 'https://github.com/Farida-Hamid/Personal-Website/blob/dev/src/style/assets/project2.png?raw=true',
   }, 
   {
   id: 3,
@@ -29,7 +29,7 @@ const Projects = () => {
   tech: ["Ruby", "Rails", "CSS", "RSPEC", "PostgreSQL"],
   liveLink: "https://budget-app-r0dc.onrender.com/users/user_id/groups",
   githubLink: "https://github.com/Farida-Hamid/BudgetApp-rails",
-  image: 'https://github.com/Farida-Hamid/Personal-Website/blob/dev/src/style/assets/project1.png?raw=true',
+  image: 'https://github.com/Farida-Hamid/Personal-Website/blob/dev/src/style/assets/project3.png?raw=true',
   },
   {
     id: 4,
@@ -38,7 +38,7 @@ const Projects = () => {
     tech: ["HTML", "Javascript", "CSS"],
     liveLink: "https://farida-hamid.github.io/Capstone/",
     githubLink: "https://github.com/Farida-Hamid/Capstone",
-    image: 'https://github.com/Farida-Hamid/Personal-Website/blob/dev/src/style/assets/project1.png?raw=true',
+    image: 'https://github.com/Farida-Hamid/Personal-Website/blob/dev/src/style/assets/project4.png?raw=true',
   }];
 
     return (
