373434b6abb6479c52d2dcc7650d433b040123b3
Farida-Hamid <faridaaddala@gmail.com>
2023-03-02 23:57:26 +0200

    Fix grammatical errors

--- src/components/About.js
+++ src/components/About.js
@@ -11,16 +11,17 @@ const About = () => {
           <div style={{width: "200px"}} >About me</div>
           <hr></hr>
         </h2>
-        <p>Hi, I’m Farida, a Full-Stack Developer who graduated from Microverse. In this 
-        remote software development program, we learn by collaborating with developers from 
-        around the world. Also, I’m a Mentor at Udacity, helping Artificial Intelligence 
-        students by reviewing projects and answering questions. <br/><br/>During my BSc in 
-        Electrical Communications and Electronics Engineering, making a ‘Hello, world!’ program was the 
-        first clue I love software. Having covered the basics, I spent the following years 
-        self-learning, and it’s been quite a journey.<br/><br/>
-        On a personal level, growing up around four different cultures established my 
-        global-mindedness, which I believe is crucial for a developer as the tech industry is 
-        so diverse.<br/><br/>Here are a few technologies I’ve been working with recently:</p>
+        <p>Hi, I’m Farida, a Full-Stack Developer who graduated from Microverse. In this
+         remote software development program, we learn by collaborating with developers
+         from around the globe. Also, I’m a Mentor at Udacity, helping Artificial
+         Intelligence students by reviewing projects and answering questions.<br/><br/>
+         During my BSc in Electrical Communications and Electronics Engineering, making a
+         ‘Hello, world!’ program was the first clue I love software. Having covered the
+         basics, I spent the following years self-learning, and it’s been quite a journey.
+         <br/><br/>On a personal level, growing up around four different cultures 
+         established my global-mindedness, which I believe is crucial for a developer as 
+         the tech industry is so diverse.<br/><br/>Here are a few technologies I’ve been 
+         working with recently:</p>
 
         <ul>
           <li> <div className="robotic">▷</div> JavaScript (ES6+)</li>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,9 +7,9 @@ const Intro = () => {
       <p className="robotic" style={{ padding: "10% 0 0"}}>Hi, my name is</p>
       <h1 style={{color: "var(--lightest-slate)"}}>Farida Hamid</h1>
       <h1 style={{color: "var(--light-slate)"}}>A Full Stack Developer</h1>
-      <p className="introduction">I'm a software engineer with an AI background. 
-      After learning so much last year, I'm longing to put it to practice and learn some more.
-      I enjoy connecting with people and build cool products.</p>
+      <p className="introduction">I'm a software engineer with an AI background. After
+       learning so much last year, I'm longing to put it into practice and learn some more.
+       I enjoy connecting with people and building cool products.</p>
     </>
   )
 }
